c30984173a1a8c9e64029640af94b1830cdc3f00
Samantha Ostrowski <isphinxs@hotmail.com>
2021-12-11 13:31:05 -0500

    Day 3 Part 2

--- days/day-3.js
+++ days/day-3.js
@@ -9,6 +9,7 @@ async function read(file) {
     return data.toString();
 };
 
+/*
 read(file)
     .then(data => {
         const values = data.split("\n");
@@ -35,6 +36,66 @@ read(file)
             };
         };
 
+        // console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
+        return parseInt(gamma, 2) * parseInt(epsilon, 2);
+    });
+*/
+
+// Day 3 Part 2
+
+read(file)
+    .then(data => {
+        const values = data.split("\n");
+
+        const findMinOrMaxChar = (arr, position, type) => {
+            let count0 = 0;
+            let count1 = 0;
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i][position] === "0") {
+                    count0++;
+                } else {
+                    count1++;
+                };
+            };
+            if (type === "max") {
+                return count0 > count1 ? "0" : "1";
+            } else {
+                return count0 > count1 ? "1" : "0";
+            };
+        };
+
+        const filterOnChar = (arr, position, char) => {
+            return arr.filter(number => {
+                return number[position] === char;
+            });
+        }
+
+        let maxArr = [...values];
+        let minArr = [...values];
+        let gamma = "";
+        let epsilon = "";
+        
+        // filter for most common digit
+        for (let i = 0; i < 12; i++) {
+            const currentMax = findMinOrMaxChar(maxArr, i, "max");
+            maxArr = filterOnChar(maxArr, i, currentMax);
+            if (maxArr.length === 1) {
+                gamma = maxArr[0];
+                break;
+            };
+        };
+        
+        // filter for least common digit
+        for (let i = 0; i < 12; i++) {
+            const currentMin = findMinOrMaxChar(minArr, i, "min");
+            minArr = filterOnChar(minArr, i, currentMin);
+            if (minArr.length === 1) {
+                epsilon = minArr[0];
+                break;
+            };
+        };
+        
+        // convert to decimal
         // console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
         return parseInt(gamma, 2) * parseInt(epsilon, 2);
     });
